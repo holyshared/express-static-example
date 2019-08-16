60e55ea6e2b4848e2365eebd4363d11d0d6deaab
holyshared <holy.shared.design@gmail.com>
2019-08-16 16:04:50 +0900

    add helmet

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import helmet from 'helmet';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import IndexPage from './components/pages/index';
 
 const app = express();
 
+app.use(helmet());
 app.use(express.static('public', {
   maxAge: process.env.ASSETS_MAX_AGE || 1 * 60 * 60 * 1000
 }));
